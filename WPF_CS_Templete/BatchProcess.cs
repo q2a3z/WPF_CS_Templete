c733439b8e484aa1f72797644d3aac5d89fb5777
Wayne_C <q2a3z@hotmail.com>
2021-07-07 01:01:12 +0900

    Templete Complete Add Command Line Excute Process End Event Handler

---
+++ WPF_CS_Templete/BatchProcess.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace WPF_CS_Templete
+{
+    class BatchProcess
+    {
+        public static String sTrMSBuildPath = @"C:\";
+        private enum FilePathType
+        { 
+            NotFound,
+            File,
+            Directory,
+        }
+        private static FilePathType GetFilePathType(string path) 
+        {
+            if (File.Exists(path)) return FilePathType.File;
+            if (Directory.Exists(path)) return FilePathType.Directory;
+            return FilePathType.NotFound;
+        }
+        public String GetMSbuildPath { get { return sTrMSBuildPath; }set { sTrMSBuildPath = value; } }
+    }
+}
